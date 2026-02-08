ed4919a31d2b91b198dab589cd029f3789a88895
Anurag Deshmukh <anuragdeshmukh61@gmail.com>
2026-02-08 18:31:48 +0530

    Add build configurations

---
+++ AIHealing/tailwind.config.js
@@ -0,0 +1,87 @@
+import { resolveProjectPath } from "wasp/dev";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: [resolveProjectPath("./src/**/*.{js,jsx,ts,tsx}")],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontSize: {
+        "tiny": ["0.625rem", "1rem"], // 10px
+      },
+      colors: {
+        // Created using https://www.tints.dev
+        primary: {
+          50: "#FFFBEB",
+          100: "#FFF7D6",
+          200: "#FFEFAD",
+          300: "#FFE680",
+          400: "#FFDA47",
+          500: "#FFCC00",
+          600: "#E6B800",
+          700: "#CCA300",
+          800: "#A88700",
+          900: "#7A6200",
+          950: "#574500"
+        },
+        // shadcn/ui theme colors
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [],
+};
